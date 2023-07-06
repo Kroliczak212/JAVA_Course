8a7d6e9c685400017caa2734757171d07ae78a60
Bartomiej Król <krolbartlomiej1122@gmail.com>
2023-07-06 20:32:25 +0200

    Getting Started

---
+++ YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("BBBB   K   K"); 
+        System.out.println("B   B  K  K"); 
+        System.out.println("B   B  K K"); 
+        System.out.println("BBBB   KK"); 
+        System.out.println("B   B  K K"); 
+        System.out.println("B   B  K  K"); 
+        System.out.println("BBBB  K   K"); 
+    }
+}
